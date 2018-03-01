fa69006246851c25b16d48a54c4319a3345ef8df
Nikita Senko <asdfg.ru@gmail.com>
2018-03-01 21:22:54 +0300

    add pagination, read data from json file

---
+++ js/grid.js
@@ -0,0 +1,75 @@
+var orders;
+jQuery.getJSON('data.json', {} ,function(json){
+    orders = json;
+});
+$(function () {
+    var dataGrid = $("#gridContainer").dxDataGrid({
+        dataSource: orders,
+        remoteOperations: {
+            sorting: true,
+            paging: true
+        },
+        paging: {
+            pageSize: 10
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [10, 20, 50],
+            showInfo: true
+        },
+        columnsAutoWidth: true,
+        filterRow: {
+            visible: true,
+            applyFilter: "auto"
+        },
+        searchPanel: {
+            visible: false,
+            width: 340,
+            placeholder: "Search..."
+        },
+        headerFilter: {
+            visible: false
+        },
+        columns: ["Position", "Name", "address", "city", "mobileNumber", "homeNumber", "email"
+        ]
+    }).dxDataGrid('instance');
+
+    var applyFilterTypes = [{
+        key: "auto",
+        name: "Immediately"
+    }, {
+        key: "onClick",
+        name: "On Button Click"
+    }];
+
+    var applyFilterModeEditor = $("#useFilterApplyButton").dxSelectBox({
+        items: applyFilterTypes,
+        value: applyFilterTypes[0].key,
+        valueExpr: "key",
+        displayExpr: "name",
+        onValueChanged: function (data) {
+            dataGrid.option("filterRow.applyFilter", data.value);
+        }
+    }).dxSelectBox("instance");
+
+    $("#filterRow").dxCheckBox({
+        text: "Filter Row",
+        value: true,
+        onValueChanged: function (data) {
+            dataGrid.clearFilter();
+            dataGrid.option("filterRow.visible", data.value);
+            applyFilterModeEditor.option("disabled", !data.value);
+        }
+    });
+
+    $("#headerFilter").dxCheckBox({
+        text: "Header Filter",
+        value: true,
+        onValueChanged: function (data) {
+            dataGrid.clearFilter();
+            dataGrid.option("headerFilter.visible", data.value);
+        }
+    });
+
+
+});
