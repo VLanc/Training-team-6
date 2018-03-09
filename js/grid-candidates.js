dd1c073c27312a5613e9df4f61abe25f32c4679a
almaguante <almaguante@gmail.com>
2018-03-09 22:59:30 +0300

    version 2. changed the table (only the required fields are displayed) "workplace"; Added the condition for displaying the time of adding candidates (today, a day ago, 2, 3, 1 week, 2 weeks, a very long time); The representation of candidates was changed: desktop - 4 tiles, tablet - 3 tiles; added animation when hovering and activating the candidate; filter candidates.

--- js/grid-candidates.js
+++ js/grid-candidates.js
@@ -1,20 +1,59 @@
 $(function () {
+
+    var newSelection = "";
+
+    $("#flavor-nav ").change(function () {
+
+        $("#grid").fadeTo(200, 0.10);
+
+        $("#flavor-nav option").removeClass("current");
+        $(this).addClass("current");
+
+        newSelection = $(this).attr("value");
+
+        $(".grid__candidate").not("." + newSelection).slideUp();
+        $("." + newSelection).slideDown();
+
+        $("#grid").fadeTo(600, 1);
+    });
+});
+
+
+$(function () {
+    var now = new Date();
+    now = now.getTime() / 1000;
     $.getJSON('data.json', function (data) {
         $.each(data, function (key, val) {
             if (!val['photo']) {
                 val['photo'] = 'anounymus';
-            };
+            }
+            ;
 
-            $('#grid').append('<div class="col-lg-2 col-md-3 col-xs-3 grid__candidate">' +
-                '<div class="grid__candidate_icon">' +
-                // '<a href="#"><img src="images/' + val['photo'] || 'anounymus' + '.png" alt="user"></a>' +
-                '<a href="#"><img src="images/' + val['photo']  + '.png" alt="user"></a>' +
+            var dateOfAddUser = (now - val['date']) / 86400;
+            if (dateOfAddUser < 1) val['date'] = 'added today';
+            else if (1 < dateOfAddUser && 2 > dateOfAddUser) val['date'] = '1 day later';
+            else if (2 < dateOfAddUser && 3 > dateOfAddUser) val['date'] = '2 day later';
+            else if (3 < dateOfAddUser && 4 > dateOfAddUser) val['date'] = '3 day later';
+            else if (7 < dateOfAddUser && 14 > dateOfAddUser) val['date'] = 'about 1 week';
+            else if (14 < dateOfAddUser && 20 > dateOfAddUser) val['date'] = 'about 2 weeks';
+            else if (21 < dateOfAddUser && 27 > dateOfAddUser) val['date'] = 'about 3 weeks';
+            else if (21 < dateOfAddUser && 27 > dateOfAddUser) val['date'] = 'this happened a long time ago :(';
+
+            $('#grid').append(
+                '<div class="col-lg-3 col-md-4 col-xs-6 grid__candidate all ' + val['status'] + '">' +
+                '<a href="#">' +
+                '<div class="grid__candidate__box">' +
+                '<div class="grid__candidate__box__icon">' +
+                '<div class="grid__candidate__box__icon__status">' + val['status'] + '</div>' +
+                '<img src="images/' + val['photo'] + '.png" alt="user">' +
                 '</div>' +
-                '<div class="grid__candidate_icon__position">' + val['Position'] +
-                '</div>' +
-                '<div class="grid__candidate_icon__name">' + val['Name'] +
-                '</div>' +
-                '<div class="grid__candidate_icon__salary">' + val['salary'] + '</div></div>');
+                '<div class="grid__candidate__box__position">' + val['Position'] + '</div>' +
+                '<div class="grid__candidate__box__name">' + val['Name'] + '</div>' +
+                '<div class="grid__candidate__box__salary">' + val['salary'] + '</div>' +
+                '<div class="grid__candidate__box__date">' + val['date'] + '</div>' +
+                '</a>' +
+                '</div>'
+            );
         });
     });
 });
