19bcb502cdf556b4bb931be22adc85d749e3d3f5
Nikita Senko <asdfg.ru@gmail.com>
2018-03-12 17:19:40 +0300

    test server

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-
 // server.get('/', restify.plugins.serveStatic({
 //     directory: __dirname,
 //     file: 'workplace.html'
@@ -19,50 +18,52 @@ server.use(restify.plugins.bodyParser());
 //     directory: __dirname
 // }));
 
-server.get('/', function (req, res, next) {
-    fs.readFile(__dirname + '/workplace.html', function (err, data) {
-        if (err) {
-            next(err);
-            return;
-        }
-
-        res.setHeader('Content-Type', 'text/html');
-        res.write(data);
-        res.end();
-        next();
-    });
-});
+// server.get('/', function (req, res, next) {
+//     fs.readFile(__dirname + '/workplace.html', function (err, data) {
+//         if (err) {
+//             next(err);
+//             return;
+//         }
+//
+//         res.setHeader('Content-Type', 'text/html');
+//         res.write(data);
+//         res.end();
+//         next();
+//     });
+// });
+//
+// server.get('/test', function (req, res, next) {
+//     fs.readFile(__dirname + '/test.html', function (err, data) {
+//         if (err) {
+//             next(err);
+//             return;
+//         }
+//
+//         res.setHeader('Content-Type', 'text/html');
+//         res.write(data);
+//         res.end();
+//         next();
+//     });
+//
+// });
 
 server.get('/test', function (req, res, next) {
-    fs.readFile(__dirname + '/test.html', function (err, data) {
-        if (err) {
-            next(err);
-            return;
-        }
 
-        res.setHeader('Content-Type', 'text/html');
-        res.write(data);
-        res.end();
-        next();
-    });
+    res.header('Access-Control-Allow-Origin', '*');
+    var entity = {
+        name : "egor"
+    };
+    res.json(entity);
+
+
 });
 
 server.post('/test', function (req, res, next) {
-    fs.readFile(__dirname + '/test.html', function (err, data) {
-        if (err) {
-            next(err);
-            return;
-        }
 
-        res.setHeader('Content-Type', 'text/html');
-        res.write(data);
-        res.end();
         console.log(req.body);
-        next();
-    });
 
 });
-//sakovski hyevo pofiksil merge conflicts
-server.listen(8000, '127.0.0.1', function () {
+
+server.listen(8000, '127.0.0.1',  function () {
     console.log('%s listening at %s', server.name, server.url);
 });
