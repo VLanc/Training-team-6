22c36124c510042358cfc42df41b5ac9d19b653e
Nikita Senko <asdfg.ru@gmail.com>
2018-03-14 18:56:47 +0300

    add editing function

--- js/id-candidate.js
+++ js/id-candidate.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $('#saveData').hide();
     $('#profedit').hide();
@@ -6,6 +7,20 @@ $(document).ready(function () {
     $('#teledit').hide();
     $('#emailedit').hide();
     $('#adressedit').hide();
+    $('#bexperience').hide();
+    $('#bkills').hide();
+
+    //закрытие модального окна
+    $('#modal_close, #overlay').click( function(){
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,  // уменьшаем прозрачность
+                function(){ // пoсле aнимaции
+                    $(this).css('display', 'none'); // скрываем окно
+                    $('#overlay').fadeOut(400); // скрывaем пoдлoжку
+                }
+            );
+    });
+
 
 });
 
@@ -20,7 +35,6 @@ function editData() {
     $('#adress').hide();
 
 
-
     $('#saveData').show();
     $('#profedit').show();
     $('#salaryedit').show();
@@ -28,6 +42,8 @@ function editData() {
     $('#teledit').show();
     $('#emailedit').show();
     $('#adressedit').show();
+    $('#bexperience').show();
+    $('#bkills').show();
 
     $('#profedit').val($('#prof').html());
     $('#salaryedit').val($('#salary').html());
@@ -35,7 +51,6 @@ function editData() {
     $('#teledit').val($('#tel').html());
     $('#emailedit').val($('#email').html());
     $('#adressedit').val($("#adress").html());
-
 
 
 }
@@ -59,31 +74,67 @@ function saveData() {
     $('#teledit').hide();
     $('#emailedit').hide();
     $('#adressedit').hide();
+    $('#bexperience').hide();
+    $('#bkills').hide();
 
     $('#prof').html($('#profedit').val());
-    $('#salary').html( $('#salaryedit').val());
+    $('#salary').html($('#salaryedit').val());
     $('#name').html($('#nameedit').val());
     $('#tel').html($('#teledit').val());
     $('#email').html($('#emailedit').val());
     $('#adress').html($('#adressedit').val());
 
 
+}
+
+function addSkill() {
+    //открываем модальное окно
+    event.preventDefault();
+    $('#overlay').fadeIn(400, // анимируем показ обложки
+        function(){ // далее показываем мод. окно
+            $('#modal_form')
+                .css('display', 'block')
+                .animate({opacity: 1, top: '50%'}, 200);
+        });
+}
+
+
+function saveSkill() {
+    var skill = $('#modalskill').val();
+    skill.toUpperCase();
+    if (skill.length > 0) {
+        $('#skills').append(
+        "<label>"+skill+"</label>"
+        );
+    }
+
+    //закрытие модального окна
+
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,  // уменьшаем прозрачность
+                function(){ // пoсле aнимaции
+                    $(this).css('display', 'none'); // скрываем окно
+                    $('#overlay').fadeOut(400); // скрывaем пoдлoжку
+                }
+            );
 
 
 
 }
+
+
 $(function () {
 
 
-  var now = new Date();
-  now = now.getTime() / 1000;
-  $.getJSON('profile.json', function (data) {
-    $.each(data, function (key, val) {
-      $('#prof').html(val['position']);
-      $('#name').html(val['name']);
-      $('#tel').html(val['mobileNumber']);
-      $('#email').html(val['email']);
-      $('#address').html(val['address']);
+    var now = new Date();
+    now = now.getTime() / 1000;
+    $.getJSON('profile.json', function (data) {
+        $.each(data, function (key, val) {
+            $('#prof').html(val['position']);
+            $('#name').html(val['name']);
+            $('#tel').html(val['mobileNumber']);
+            $('#email').html(val['email']);
+            $('#address').html(val['address']);
+        });
     });
-  });
 });
