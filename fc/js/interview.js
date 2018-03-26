3b7dd20f3ac318d9e56334885a9463741832b719
VLanc <vlancv@gmail.com>
2018-03-27 00:12:14 +0300

    #37 Fixed some bugs in the creating an event function

--- fc/js/interview.js
+++ fc/js/interview.js
@@ -17,11 +17,26 @@ $(document).ready(function () {
     weekNumberCalculation: 'ISO',
     /*editable: true,*/
     eventLimit: true, // allow "more" link when too many events
-    /*selectable: true,*/
+    selectable: true,
     /*selectHelper: true,*/
     eventSources: [
       'fc/event.json'
-    ]
+    ],
+    dayClick: function (date, jsEvent, view) {
+      $('#exampleModalCenter').modal('show');
+      $("#event-start-date").val("" + date.format() + " 00:00");
+      $("#event-end-date").val("" + date.format() + " 23:59");
+    }
+    /*    eventClick: function(calEvent, jsEvent, view) {
+
+          console.log('Event: ' + calEvent.title);
+          console.log('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+          console.log('View: ' + view.name);
+
+          $(this).css('border-color', 'red');
+
+        }*/
+
 
   });
   // create object
@@ -43,27 +58,75 @@ $(document).ready(function () {
     });*/
 
 
-  $("#save-event-button").click(function () {
-
-    $("#save-event-button").submit(function (e) {
-      e.preventDefault();
-    });
+  $("#save-event-button").click(function (e) {
 
     var eventTitle = $("#event-title").val();
     var eventStartDate = $("#event-start-date").val().replace(" ", "T");
     var eventEndDate = $("#event-end-date").val().replace(" ", "T");
+    var eventParticipantIndex = $("#select-participant option:selected").val();
     var eventParticipant = $("#select-participant option:selected").html();
     eventTitle += " - " + eventParticipant;
-    if (!eventTitle || !eventStartDate || !eventEndDate || !eventParticipant) return;
+    if (!eventTitle || !eventStartDate || !eventEndDate || !eventParticipantIndex) return;
 
     var event = {title: eventTitle, start: eventStartDate, end: eventEndDate};
 
     $('#calendar').fullCalendar('renderEvent', event, true);
 
+    e.preventDefault();
+    clearModalWindow();
+  });
+
+  function clearModalWindow() {
     $("#event-title").val("");
     $("#event-start-date").val("");
     $("#event-end-date").val("");
-    $("#select-participant").val(0);
+    $("#select-participant").val($("#select-participant option:first").val());
+  }
 
+  $("#close-event-modal-button-top").click(function () {
+    clearModalWindow();
   });
+
+  $("#close-event-modal-button-bottom").click(function () {
+    clearModalWindow();
+  });
+
+  var isCalendarForStartHidden = true;
+  $("#show-calendar-button-for-start").click(function () {
+    if (isCalendarForStartHidden) {
+      $("#show-calendar-button-for-start").html("Hide calendar");
+      isCalendarForStartHidden = false;
+    } else {
+      $("#show-calendar-button-for-start").html("Show calendar");
+      isCalendarForStartHidden = true;
+    }
+
+    $(".event-start-datepicker").slideToggle("slow");
+  });
+
+  $(".event-start-datepicker").datepicker({
+    onSelect: function onSelect(date) {
+      $("#event-start-date").val(date);
+    }
+  });
+
+  var isCalendarForEndHidden = true;
+  $("#show-calendar-button-for-end").click(function () {
+    if (isCalendarForEndHidden) {
+      $("#show-calendar-button-for-end").html("Hide calendar");
+      isCalendarForEndHidden = false;
+    } else {
+      $("#show-calendar-button-for-end").html("Show calendar");
+      isCalendarForEndHidden = true;
+    }
+
+    $(".event-end-datepicker").slideToggle("slow");
+  });
+
+  $(".event-end-datepicker").datepicker({
+    onSelect: function onSelect(date) {
+      $("#event-end-date").val(date);
+    }
+  });
+
 });
