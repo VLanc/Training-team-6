7dd2c18a3564bd4018fa4769cad8f9e94688cfc1
Nikita Senko <asdfg.ru@gmail.com>
2018-03-27 01:50:52 +0300

    fix

--- fc/js/interview.js
+++ fc/js/interview.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -19,15 +17,45 @@ $(document).ready(function () {
         weekNumberCalculation: 'ISO',
         /*editable: true,*/
         eventLimit: true, // allow "more" link when too many events
-        /*selectable: true,*/
+        selectable: true,
         /*selectHelper: true,*/
-        // eventSources: [
-        //     'fc/event.json'
-        // ],
         events: {
             url: 'http://127.0.0.1:8080/interview'
         },
-        $("#save-event-button").click(function (e) {
+        dayClick: function (date, jsEvent, view) {
+            $('#exampleModalCenter').modal('show');
+            $("#event-start-date").val("" + date.format() + " 00:00");
+            $("#event-end-date").val("" + date.format() + " 23:59");
+        }
+        /*    eventClick: function(calEvent, jsEvent, view) {
+              console.log('Event: ' + calEvent.title);
+              console.log('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+              console.log('View: ' + view.name);
+              $(this).css('border-color', 'red');
+            }*/
+
+
+    });
+    // create object
+    // var events = JSON.parse('event.json', 'utf8');
+    /*  $('#dialog').dialog({
+        autoOpen: false,
+        minWidth: 600,
+        show: {
+          effect: 'drop',
+          duration: 500
+        },
+        hide: {
+          effect: 'clip',
+          duration: 500
+        }
+      });
+      $('.datepicker').datepicker({
+        dateFormat: "yy-mm-dd"
+      });*/
+
+
+    $("#save-event-button").click(function (e) {
 
         var eventTitle = $("#event-title").val();
         var eventStartDate = $("#event-start-date").val().replace(" ", "T");
@@ -37,14 +65,9 @@ $(document).ready(function () {
         eventTitle += " - " + eventParticipant;
         if (!eventTitle || !eventStartDate || !eventEndDate || !eventParticipantIndex) return;
 
-        var eventTitle = $("#event-title").val();
-        var eventStartDate = $("#event-start-date").val().replace(" ", "T");
-        var eventEndDate = $("#event-end-date").val().replace(" ", "T");
-        var eventParticipant = $("#select-participant option:selected").html();
-        eventTitle += " - " + eventParticipant;
-        if (!eventTitle || !eventStartDate || !eventEndDate || !eventParticipant) return;
-
         var event = {title: eventTitle, start: eventStartDate, end: eventEndDate};
+
+        $('#calendar').fullCalendar('renderEvent', event, true);
 
         e.preventDefault();
         clearModalWindow();
@@ -104,4 +127,3 @@ $(document).ready(function () {
     });
 
 });
-
