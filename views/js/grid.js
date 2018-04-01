9384bb4f282646b4c74589505539135cd98b9aab
Nikita Senko <asdfg.ru@gmail.com>
2018-04-01 23:14:19 +0300

    deploy

--- views/js/grid.js
+++ views/js/grid.js
@@ -1,104 +1,109 @@
+var orders;
+var url = "/vacancies-grid";
+$.getJSON(url, function (data) {
+
+
+  orders = data;
+
+
+});
 $(document).ready(function () {
-    var orders;
-    var url = "/vacancies-grid";
-    $.getJSON(url, function (data) {
+    $(function () {
 
-
-        orders = data;
-
-
-    });
-
-    var dataGrid = $("#gridContainer").dxDataGrid({
-        dataSource: orders,
-        remoteOperations: {
-            sorting: true,
-            paging: true
-        },
-        paging: {
-            pageSize: 10
-
-        },
-        pager: {
-            showPageSizeSelector: true,
-            allowedPageSizes: [10, 20, 50],
-            showNavigationButtons: true
-        },
-
-        columnsAutoWidth: true,
-        filterRow: {
-            visible: true,
-            applyFilter: "auto"
-        },
-        searchPanel: {
-            visible: false,
-            width: 340,
-            placeholder: "Search..."
-        },
-
-        columnMinWidth: 100,
-
-
-        headerFilter: {
-            visible: false
-        },
-
-        columns: ["position",
-            "name",
-            "address",
-            "city",
-            {
-                dataField: "mobileNumber",
-                caption: "mobileNumber",
-                headerFilter: {
-                    groupInterval: 10000
-                }
+        var dataGrid = $("#gridContainer").dxDataGrid({
+            dataSource: orders,
+            remoteOperations: {
+                sorting: true,
+                paging: true
             },
-            {
-                dataField: "homeNumber",
-                caption: "homeNumber",
-                headerFilter: {
-                    groupInterval: 10000
-                }
+            paging: {
+                pageSize: 10
+
             },
-            "email"
-        ]
-    }).dxDataGrid('instance');
+            pager: {
+                showPageSizeSelector: true,
+                allowedPageSizes: [10, 20, 50],
+                showNavigationButtons: true
+            },
 
-    var applyFilterTypes = [{
-        key: "auto",
-        name: "Immediately"
-    }, {
-        key: "onClick",
-        name: "On Button Click"
-    }];
+            columnsAutoWidth: true,
+            filterRow: {
+                visible: true,
+                applyFilter: "auto"
+            },
+            searchPanel: {
+                visible: false,
+                width: 340,
+                placeholder: "Search..."
+            },
 
-    var applyFilterModeEditor = $("#useFilterApplyButton").dxSelectBox({
-        items: applyFilterTypes,
-        value: applyFilterTypes[0].key,
-        valueExpr: "key",
-        displayExpr: "name",
-        onValueChanged: function (data) {
-            dataGrid.option("filterRow.applyFilter", data.value);
-        }
-    }).dxSelectBox("instance");
+            columnMinWidth: 100,
 
-    $("#filterRow").dxCheckBox({
-        text: "Filter Row",
-        value: true,
-        onValueChanged: function (data) {
-            dataGrid.clearFilter();
-            dataGrid.option("filterRow.visible", data.value);
-            applyFilterModeEditor.option("disabled", !data.value);
-        }
+
+            headerFilter: {
+                visible: false
+            },
+
+            columns: ["position",
+                "name",
+                "address",
+                "city",
+                {
+                    dataField: "mobileNumber",
+                    caption: "mobileNumber",
+                    headerFilter: {
+                        groupInterval: 10000
+                    }
+                },
+                {
+                    dataField: "homeNumber",
+                    caption: "homeNumber",
+                    headerFilter: {
+                        groupInterval: 10000
+                    }
+                },
+                "email"
+            ]
+        }).dxDataGrid('instance');
+
+        var applyFilterTypes = [{
+            key: "auto",
+            name: "Immediately"
+        }, {
+            key: "onClick",
+            name: "On Button Click"
+        }];
+
+        var applyFilterModeEditor = $("#useFilterApplyButton").dxSelectBox({
+            items: applyFilterTypes,
+            value: applyFilterTypes[0].key,
+            valueExpr: "key",
+            displayExpr: "name",
+            onValueChanged: function (data) {
+                dataGrid.option("filterRow.applyFilter", data.value);
+            }
+        }).dxSelectBox("instance");
+
+        $("#filterRow").dxCheckBox({
+            text: "Filter Row",
+            value: true,
+            onValueChanged: function (data) {
+                dataGrid.clearFilter();
+                dataGrid.option("filterRow.visible", data.value);
+                applyFilterModeEditor.option("disabled", !data.value);
+            }
+        });
+
+        $("#headerFilter").dxCheckBox({
+            text: "Header Filter",
+            value: true,
+            onValueChanged: function (data) {
+                dataGrid.clearFilter();
+                dataGrid.option("headerFilter.visible", data.value);
+            }
+        });
+
+
     });
 
-    $("#headerFilter").dxCheckBox({
-        text: "Header Filter",
-        value: true,
-        onValueChanged: function (data) {
-            dataGrid.clearFilter();
-            dataGrid.option("headerFilter.visible", data.value);
-        }
-    });
 });
