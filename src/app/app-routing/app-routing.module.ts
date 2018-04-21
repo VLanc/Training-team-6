03473303252a31bf8f05981cf8eb48267209d9b1
Nikita Senko <asdfg.ru@gmail.com>
2018-04-21 16:01:47 +0300

    fix

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -14,7 +14,7 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [
     // RouterModule.forRoot(appRoutes)
-    RouterModule.forRoot(appRoutes, { enableTracing: true })// <-- debugging purposes only,
+    RouterModule.forRoot(appRoutes)// <-- debugging purposes only,
   ],
   exports: [RouterModule]
 })
