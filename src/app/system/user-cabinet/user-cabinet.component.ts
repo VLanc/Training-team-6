4a8d692a1608a52d4169900e0d515d52fca98d34
Nikita Senko <asdfg.ru@gmail.com>
2018-04-25 03:59:09 +0300

    interview complete:

--- src/app/system/user-cabinet/user-cabinet.component.ts
+++ src/app/system/user-cabinet/user-cabinet.component.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {User} from '../../shared/models/user.model';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
-// import {FormGroup, Validators} from '@angular/forms';
-// import {Message} from '../../shared/models/message.model';
-
 
 @Component({
   selector: 'app-user-cabinet',
@@ -12,12 +9,13 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./user-cabinet.component.css']
 })
 export class UserCabinetComponent implements OnInit {
-  // message: Message;
   form: FormGroup;
   user: User;
-
+  private textForEmptyField = 'You haven\`t filled this field yet';
+  private textErrorForNames = 'field is empty or less than two characters';
+  private textErrorForRole = 'field is empty or less than two characters';
+  private textErrorForEmail = 'field is empty or is not email';
   private editing = true;
-  private allowedToSave = true;
 
 
   constructor() {
@@ -29,27 +27,39 @@ export class UserCabinetComponent implements OnInit {
 
 
   saveProfile(): void {
+    this.processingRole();
     for (const field in this.user) {
       if (this.user.hasOwnProperty(field) && this.user[field]) {
         this.user[field].trim();
-        console.log(field + ' - ' + this.user[field]);
       }
     }
     window.localStorage.setItem('user', JSON.stringify(this.user));
   }
 
+  onSubmit(): void {
+    this.saveProfile();
+    this.editing = true;
+  }
+
+  processingRole() {
+    /*TODO this is a hardcode*/
+    if (this.user.roleIndex === '1') {
+      return 'developer';
+    } else {
+      return 'manager';
+    }
+    /*END of hardcode*/
+  }
 
   ngOnInit() {
-
     this.user = JSON.parse(window.localStorage.getItem('user'));
-    console.log(this.user);
-    console.log(this.user.name);
-
-//     this.userObject = JSON.parse(window.localStorage.getItem('user'));
-//     this.form = new FormGroup({
-//       'name': new FormControl(null, [Validators.required, Validators.email])
-//       // 'password': new FormControl(null, [Validators.required, Validators.minLength(3)])
-//     });
+    this.processingRole();
+    this.form = new FormGroup({
+      'name': new FormControl(null, [Validators.required, Validators.minLength(2)]),
+      'surname': new FormControl(null, [Validators.required, Validators.minLength(2)]),
+      'roleIndex': new FormControl(null, [Validators.required, Validators.minLength(1)]),
+      'email': new FormControl(null, [Validators.required, Validators.email])
+    });
 // >>>>>>> 563fe1275cac8c098d170352210ccda1103371cc
   }
 }
