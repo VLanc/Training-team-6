987c747ad87227afe03cc78d022411987ab9049e
Nikita Senko <asdfg.ru@gmail.com>
2018-05-02 20:56:38 +0300

    add apibase class

--- src/app/system/shared/services/candidates.service.ts
+++ src/app/system/shared/services/candidates.service.ts
@@ -1,19 +1,25 @@
-
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Candidate} from '../models/candidate.model';
+import {BaseApi} from '../../../shared/core/base-api';
 
 @Injectable()
-export class CandidatesService {
-  constructor(private http: HttpClient){
+export class CandidatesService extends BaseApi{
+  url = 'http://localhost:8080/';
+  constructor(public http: HttpClient){
+    super(http);
   }
 
-  url = 'http://localhost:8080/';
+
   getCandidates():Observable<Candidate[]>{
     return this.http.get<Candidate[]>(this.url+'candidates')
       .map(data => {
         return data;
       });
   }
+
+  // getCandidates():Observable<Candidate[]>{
+  //   return this.get('candidates');
+  // }
 }
